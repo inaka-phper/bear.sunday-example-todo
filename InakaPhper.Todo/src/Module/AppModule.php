3fae987aa28ff7197b9c9b76e1d562cdb35f9c87
phper <inaka-phper@users.noreply.github.com>
2015-11-24 01:36:19 +0900

    [add]add tutorial "DI - monolog" settings.
    looking for "http://bearsunday.github.io/manuals/1.0/ja/tutorial.html"

--- InakaPhper.Todo/src/Module/AppModule.php
+++ InakaPhper.Todo/src/Module/AppModule.php
@@ -4,7 +4,9 @@ namespace MyVendor\MyProject\Module;
 
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
+use Psr\Log\LoggerInterface;
 use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
 
 class AppModule extends AbstractModule
 {
@@ -15,5 +17,6 @@ class AppModule extends AbstractModule
     {
         $this->install(new PackageModule);
         $this->override(new AuraRouterModule);
+        $this->bind(LoggerInterface::class)->toProvider(MonologLoggerProvider::class)->in(Scope::SINGLETON);
     }
 }
